1807053740fcbdfce11fe9571211af2621fe0dc8
Alex Maximiano <n4ow.dev@gmail.com>
2021-12-27 14:36:08 -0300

    User entity and resource

---
+++ src/main/java/com/alexmaximiano/curso/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.alexmaximiano.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alexmaximiano.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Alex", "alex@gmail.com", "14997642643", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
---
+++ src/test/java/com/alexmaximiano/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexmaximiano.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
